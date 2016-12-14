051620a3d1a4fb6bfbc926b5d3637cec10d1b498
TonnamP <mytjn14@gmail>
2016-12-14 15:33:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+<div id="app" v-show="App">
+  <hello></hello>
+  <button type="button" name="GoToRead" class="button button" @click="Readsheet(1)">Go To Read</button>
+  <button type="button" name="UpLoad" class="button button" @click="Readsheet(2)">Upload</button>
+</div>
+<div v-show="gotoread" :readsheet="Readsheet">
+  <read-sheet></read-sheet>
+</div>
+<div v-show="uploadsheet">
+  <upload-sheet></upload-sheet>
+</div>
+</div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import ReadSheet from './components/ReadSheet'
+import UploadSheet from './components/UploadSheet'
+export default {
+  name: 'app',
+  data () {
+    return {
+      gotoread: false,
+      uploadsheet: false,
+      App: true
+    }
+  },
+  components: {
+    Hello,
+    ReadSheet,
+    UploadSheet
+  },
+  methods: {
+    Readsheet (page) {
+      if (page === 1) {
+        this.gotoread = true
+        this.App = false
+        this.uploadsheet = false
+      } else if (page === 2) {
+        this.uploadsheet = true
+        this.gotoread = false
+        this.App = false
+      }
+    }
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Raleway');
+
+body {
+  font-family: 'Raleway', sans-serif;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 270px;
+}
+
+.button {
+  background-color: #000000;
+  /* Green */
+  border: none;
+  border-radius: 8px;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s;
+  /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+  width: 200px;
+  height: 50px;
+}
+</style>
---
+++ src/components/ReadSheet.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="bgcolor">
+  <ul>
+    <li><a class="active" @click="ReadSheet(2)">Home</a></li>
+    <li><input type="search" class="search"></li>
+    <li><a @click="ReadSheet(2)">Upload</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
+  <div class="form-style-6">
+    <h1>Network Programing</h1>
+    <form>
+      <input type="submit" value="Read More >" class="canup"/>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['ReadSheet'],
+  name: 'ReadSheet',
+  data () {
+    return {
+      gotoupload: false
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Raleway');
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #F8F8FF;
+  color: #000000;
+}
+
+.active {
+  background-color: #333;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.search {
+  border-radius: 8px;
+  width: 230px;
+  height: 30px;
+  margin-top: 8px;
+  margin-left: 770px;
+}
+
+.form-style-6{
+    font-family: 'Raleway', sans-serif;
+    max-width: 400px;
+    height: 120px;
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-right: 20px;
+    padding: 20px;
+    background: #F8F8FF;
+    box-shadow: 1px 1px 1px 0.5px #DCDCDC;
+}
+.form-style-6 h1{
+    background: #000000;
+    padding: 20px 0;
+    font-size: 230%;
+    font-weight: 300;
+    text-align: center;
+    color: #fff;
+    margin: -16px -16px 16px -16px;
+}
+.canup{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    width: 50%;
+    padding: 3%;
+    background: #000000;
+    margin-bottom: 5px;
+    color: #ffffff;
+    font-family: 'Raleway', sans-serif;
+    display: inline-block;
+}
+.canup:hover,
+.canup:hover{
+    background: #ffffff;
+    color: #000000;
+}
+.bgcolor {
+  background-color: #F5F5F5;
+  width: 100vu;
+  height: 100vh;
+}
+</style>
